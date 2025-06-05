5809f1670d0dac8b3a960e17e27482c392734905
St-Bra <st.braitsau@gmail.com>
2025-06-05 16:55:13 +0200

    add all models

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class BotUser(models.Model):
+    telegram_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=150, blank=True, null=True)
+    language_code = models.CharField(
+        max_length=2,
+        choices=[
+            ('ru', 'Russian'),
+            ('en', 'English'),
+            ('pl', 'Polish'),
+
+        ],
+        default='en'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username or str(self.telegram_id)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,25 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from forecast.models import Currency
 
-# Create your models here.
+class NewsSource(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class NewsArticle(models.Model):
+    title = models.CharField(max_length=500)
+    content = models.TextField()
+    published_at = models.DateTimeField()
+    url = models.URLField(unique=True)
+    source = models.ForeignKey(NewsSource, on_delete=models.CASCADE, related_name='articles')
+    related_currencies = models.ManyToManyField(Currency, related_name='news_articles')
+    sentiment_score = models.FloatField(
+        null=True, blank=True,
+        validators=[MinValueValidator(-1.0), MaxValueValidator(1.0)]
+    )
+    def __str__(self):
+        return self.title
